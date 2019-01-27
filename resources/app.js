d0f14915b3de349bb186e408e2782a119e5714b7
baadillahnabil <nabilzero27@gmail.com>
2019-01-27 20:03:01 +0700

    add search. make search responsive.

--- resources/app.js
+++ resources/app.js
@@ -1,8 +1,23 @@
-import 'jquery/dist/jquery.slim.min.js'
+import * as $ from 'jquery/dist/jquery.min.js'
 import 'popper.js/dist/popper.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
-const yeah = 'yeah'
-const app = () => {
-  console.log(`oh ${yeah}`)
-}
+const primaryColor = '#40a9bc'
+
+$(document).ready(() => {
+  $('.searchToggleTrigger').click(() => {
+    if ($('#formSearch').css('display') === 'none') {
+      $('#formSearch').removeClass(['d-lg-none', 'd-xl-none'])
+      $('#navbarMenus').css('display', 'none')
+
+      $('.searchToggleTrigger > .fas').css('color', primaryColor)
+
+      $('#searchInput').focus()
+    } else {
+      $('#formSearch').addClass(['d-lg-none', 'd-xl-none'])
+      $('#navbarMenus').css('display', 'flex')
+
+      $('.searchToggleTrigger > .fas').css('color', 'white')
+    }
+  })
+})

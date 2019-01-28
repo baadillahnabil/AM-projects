76af358a0f70dd6b0ceddd1eed42d59abc22c9ab
baadillahnabil <nabilzero27@gmail.com>
2019-01-28 21:29:10 +0700

    add Paragraps, Simple CTA and IMage Content

--- resources/app.js
+++ resources/app.js
@@ -1,6 +1,7 @@
 import * as $ from 'jquery/dist/jquery.min.js'
 import 'popper.js/dist/popper.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import 'zoom-vanilla.js/dist/zoom-vanilla.min.js'
 
 const primaryColor = '#40a9bc'
 

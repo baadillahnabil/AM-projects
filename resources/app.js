4d1614c71488692d8e0163f5d9f5c08e03b5a084
baadillahnabil <nabilzero27@gmail.com>
2019-02-01 02:30:11 +0700

    implement slick carousel. implement arrows. styling.

--- resources/app.js
+++ resources/app.js
@@ -2,6 +2,7 @@ window.$ = window.jQuery = require('jquery/dist/jquery.min.js')
 import 'popper.js/dist/popper.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import 'magnific-popup/dist/jquery.magnific-popup.min.js'
+import 'slick-carousel/slick/slick.min.js'
 
 const primaryColor = '#40a9bc'
 
@@ -17,6 +18,64 @@ $(document).ready(() => {
 
   $('.image_content').magnificPopup({
     type: 'image'
+  })
+
+  $('.similar-items__cards-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+    mobileFirst: true,
+    adaptiveHeight: true,
+    appendDots: $('.similar-items__cards-dots'),
+    customPaging: (slider, index) => {
+      return '<img src="./dist/icons/dot_solid_primary.svg" />'
+    },
+    prevArrow: $('.similar-items__cards-arrows > .arrow-left'),
+    nextArrow: $('.similar-items__cards-arrows > .arrow-right'),
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 75,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true
+        }
+      }
+    ]
   })
 })
 

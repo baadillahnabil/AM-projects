4a914f9ac0a2aa1b4eed8379b398342a746125a0
baadillahnabil <nabilzero27@gmail.com>
2019-02-11 22:15:47 +0700

    add content for step 2

--- project-3/resources/app.js
+++ project-3/resources/app.js
@@ -2,4 +2,6 @@ window.$ = window.jQuery = require('jquery/dist/jquery.min.js')
 import 'popper.js/dist/popper.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
-$(document).ready(() => {})
+$(document).ready(() => {
+  $('[data-toggle="tooltip"]').tooltip() // enable bootstrap tooltip
+})

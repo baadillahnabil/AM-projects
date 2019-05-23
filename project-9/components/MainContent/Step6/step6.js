2b0411cbe5ddbaea00af43cab224f1a386001bfc
baadillahnabil <nabilzero27@gmail.com>
2019-05-23 16:35:44 +0700

    work on step 6. make it responsive

--- project-9/components/MainContent/Step6/step6.js
+++ project-9/components/MainContent/Step6/step6.js
@@ -1,35 +1,22 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // On Selection Changed
-  $('#step6-form input[name="step6-form"]').on('change', () => {
-    const selected = $('#step6-form input[name="step6-form"]:checked').val() !== undefined
-
-    if (selected) {
-      $('#step6-skip-button').fadeOut(200)
-      $('#step6-chat-bubble').fadeOut(200, () => {
-        $('#step6-next-button').removeClass('d-none')
-        $('#step6-next-button').css('display', '')
-        $('#step6-skip-button').addClass('d-none')
-        $('#step6-chat-bubble').addClass('d-none')
-      })
-    } else {
-      $('#step6-next-button').fadeOut(200, () => {
-        $('#step6-skip-button').removeClass('d-none')
-        $('#step6-chat-bubble').removeClass('d-none')
-        $('#step6-skip-button').css('display', '')
-        $('#step6-chat-bubble').css('display', '')
-        $('#step6-next-button').addClass('d-none')
-      })
-    }
-  })
+  // TODO : Validation
 
   // On Next or Skip Button Clicked
   $('#step6-next-button').on('click', () => {
-    goToStep7()
-  })
-  $('#step6-skip-button').on('click', () => {
-    goToStep7()
+    $('#hic__step6').addClass('animated faster fadeOut')
+    $('#hic__step6').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
+      $('#hic__step6').addClass('d-none')
+      $('#hic__step6').removeClass('animated faster fadeOut')
+
+      $('#hic__step7').removeClass('d-none')
+      $('#hic__step7').addClass('animated faster fadeIn')
+
+      $('#hic__step7').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
+        $('#hic__step7').removeClass('animated faster fadeIn')
+      })
+    })
   })
 
   // On Back Button Clicked
@@ -47,18 +34,3 @@ $(document).ready(() => {
     })
   })
 })
-
-function goToStep7() {
-  $('#hic__step6').addClass('animated faster fadeOut')
-  $('#hic__step6').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
-    $('#hic__step6').addClass('d-none')
-    $('#hic__step6').removeClass('animated faster fadeOut')
-
-    $('#hic__step7').removeClass('d-none')
-    $('#hic__step7').addClass('animated faster fadeIn')
-
-    $('#hic__step7').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
-      $('#hic__step7').removeClass('animated faster fadeIn')
-    })
-  })
-}

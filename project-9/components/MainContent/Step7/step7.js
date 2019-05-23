9a8b6eb625afe7abe205533055bd50938b9658b4
baadillahnabil <nabilzero27@gmail.com>
2019-05-23 23:18:57 +0700

    implement search-select. edit chat on value changed.

--- project-9/components/MainContent/Step7/step7.js
+++ project-9/components/MainContent/Step7/step7.js
@@ -1,66 +1,73 @@
 import $ from 'jquery'
+import 'popper.js'
+import 'bootstrap'
+import 'bootstrap-select'
 
 $(document).ready(() => {
-  // Check Validation
-  $('#currentFund').on('change', () => {
-    isSelectValid()
+  // Init dropdown menu
+  $('#step7-form__occupation').selectpicker({
+    liveSearchPlaceholder: 'Typing...'
+  })
 
-    const value = $('#currentFund').val()
-    if (value === 'not insured') {
-      $('#step7-chat-bubble .message').html('No worries, we will find suitable health insurance cover for you.')
-    } else {
-      $('#step7-chat-bubble .message').html(
-        'We will take into account your current fund when putting together your health insurance quotes.'
+  // render "I can’t find my occupation" to dropdown menu
+  $('#step7-form__occupation').on('loaded.bs.select', () => {
+    $('.lic__step7 .bootstrap-select .dropdown-menu').append($('#step7-no-selection'))
+  })
+
+  // on value changed
+  $('#step7-form__occupation').on('changed.bs.select', (event, clickedIndex, isSelected, previousValue) => {
+    const value = event.target.value
+
+    if (value !== '__none__') {
+      $('#step7-chat-1').html('The risk profile of your job can increase the price of your premium.')
+      $('#step7-chat-2').html(
+        "That's okay. We'll quote you as an <b>Office Worker </b>for now, but you can change this later."
       )
+      $('#step7-chat-2').addClass('d-none')
     }
+  })
+
+  // on "I can’t find my occupation" clicked
+  $('#step7-no-selection').on('click', () => {
+    $('#step7-form__occupation').val('__none__')
+    $('#step7-form__occupation').selectpicker('render')
+
+    // Show additional chat
+    const chat1 = $('#step7-chat-1').html()
+    const chat2 = $('#step7-chat-2').html()
+    $('#step7-chat-1').html(chat2)
+    $('#step7-chat-2').html(chat1)
+    $('#step7-chat-2').removeClass('d-none')
   })
 
   // On Next or Skip Button Clicked
   $('#step7-next-button').on('click', () => {
-    if (!isSelectValid()) return
-    goToStep8()
+    $('#lic__step7').addClass('animated faster fadeOut')
+    $('#lic__step7').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
+      $('#lic__step7').addClass('d-none')
+      $('#lic__step7').removeClass('animated faster fadeOut')
+
+      $('#lic__step8').removeClass('d-none')
+      $('#lic__step8').addClass('animated faster fadeIn')
+
+      $('#lic__step8').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
+        $('#lic__step8').removeClass('animated faster fadeIn')
+      })
+    })
   })
 
   // On Back Button Clicked
   $('#step7-back-button').on('click', () => {
-    $('#hic__step7').addClass('animated faster fadeOut')
-    $('#hic__step7').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
-      $('#hic__step7').addClass('d-none')
-      $('#hic__step7').removeClass('animated faster fadeOut')
+    $('#lic__step7').addClass('animated faster fadeOut')
+    $('#lic__step7').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
+      $('#lic__step7').addClass('d-none')
+      $('#lic__step7').removeClass('animated faster fadeOut')
 
-      $('#hic__step6').removeClass('d-none')
-      $('#hic__step6').addClass('animated faster fadeIn')
-      $('#hic__step6').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
-        $('#hic__step6').removeClass('animated faster fadeIn')
+      $('#lic__step6').removeClass('d-none')
+      $('#lic__step6').addClass('animated faster fadeIn')
+      $('#lic__step6').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
+        $('#lic__step6').removeClass('animated faster fadeIn')
       })
     })
   })
 })
-
-function goToStep8() {
-  $('#hic__step7').addClass('animated faster fadeOut')
-  $('#hic__step7').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
-    $('#hic__step7').addClass('d-none')
-    $('#hic__step7').removeClass('animated faster fadeOut')
-
-    $('#hic__step8').removeClass('d-none')
-    $('#hic__step8').addClass('animated faster fadeIn')
-
-    $('#hic__step8').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
-      $('#hic__step8').removeClass('animated faster fadeIn')
-    })
-  })
-}
-
-function isSelectValid() {
-  const isSelected = $('#currentFund option:selected').val() !== ''
-  if (!isSelected) {
-    $('#currentFund').addClass('error-state')
-    $('#currentFund + .info').removeClass('d-none')
-  } else {
-    $('#currentFund').removeClass('error-state')
-    $('#currentFund + .info').addClass('d-none')
-  }
-
-  return isSelected
-}

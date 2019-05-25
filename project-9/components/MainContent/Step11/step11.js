be397218c22d53176892bc88f89a026cb8f4327b
baadillahnabil <nabilzero27@gmail.com>
2019-05-25 16:51:12 +0700

    Add Step 8, make it responsive, add validation on it

---
+++ project-9/components/MainContent/Step11/step11.js
@@ -0,0 +1,100 @@
+import $ from 'jquery'
+
+$(document).ready(() => {
+  // Validation on Email Address
+  $('#step11-form__email input').on('input', () => {
+    isEmailAddressValid()
+  })
+
+  // Validation on Phone
+  $('#step11-form__phone input').on('input', () => {
+    isPhoneValid()
+  })
+
+  // On Next Button Clicked
+  $('#step11-next-button').on('click', () => {
+    // Check Validation
+    isEmailAddressValid()
+    isPhoneValid()
+
+    if (isEmailAddressValid() && isPhoneValid()) {
+      goToStep12()
+    }
+  })
+})
+
+function goToStep12() {
+  $('#hic__step11').addClass('animated faster fadeOut')
+  $('#hic__step11').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
+    $('#hic__step11').addClass('d-none')
+    $('#hic__step11').removeClass('animated faster fadeOut')
+
+    $('#hic__step12').removeClass('d-none')
+    $('#hic__step12').addClass('animated faster fadeIn')
+
+    $('#hic__step12').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
+      $('#hic__step12').removeClass('animated faster fadeIn')
+
+      step12ProgressAnimation()
+    })
+  })
+}
+
+// Rule:
+// ------
+// 1. Should match this regex  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+function isEmailAddressValid() {
+  const emailAddressRegEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+  if (!emailAddressRegEx.test($('#step11-form__email input').val())) {
+    $('#step11-form__email').addClass('error-state')
+    $('#step11-form__email + .info').removeClass('d-none')
+
+    return false
+  } else {
+    $('#step11-form__email').removeClass('error-state')
+    $('#step11-form__email + .info').addClass('d-none')
+
+    return true
+  }
+}
+
+// Rule:
+// ------
+// 1. Should match this regex  /^0(2|3|4|7|8){1}[0-9]{8}$/
+function isPhoneValid() {
+  const phoneRegEx = /^0(2|3|4|7|8){1}[0-9]{8}$/
+
+  if (!phoneRegEx.test($('#step11-form__phone input').val())) {
+    $('#step11-form__phone').addClass('error-state')
+    $('#step11-form__phone + .info').removeClass('d-none')
+
+    return false
+  } else {
+    $('#step11-form__phone').removeClass('error-state')
+    $('#step11-form__phone + .info').addClass('d-none')
+
+    return true
+  }
+}
+
+function step12ProgressAnimation() {
+  setTimeout(() => {
+    $('.hic__step12 .progress .progress-bar').css('width', '0')
+    $('.hic__step12 .progress .progress-bar').css('width', '30%')
+    setTimeout(() => {
+      $('.hic__step12 .progress .progress-bar').css('width', '70%')
+      setTimeout(() => {
+        $('.hic__step12 .progress .progress-bar').css('width', '100%')
+
+        // on progress complete
+        $('#step12-firstTitle').addClass('d-none')
+        $('#step12-secondTitle').removeClass('d-none')
+        $('#step12-loading .success').removeClass('d-none')
+        $('#step12-next-button').removeClass('d-none')
+        $('#step12-loading .loading').addClass('d-none')
+        $('#step12-loading .message').html('Analysis complete')
+      }, 800)
+    }, 1000)
+  }, 500)
+}
--- project-9/components/MainContent/Step8/step8.js
+++ project-9/components/MainContent/Step8/step8.js
@@ -1,100 +1,56 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // Validation on Email Address
-  $('#step8-form__email input').on('input', () => {
-    isEmailAddressValid()
-  })
-
-  // Validation on Phone
-  $('#step8-form__phone input').on('input', () => {
-    isPhoneValid()
+  // Check Validation
+  $('#step8-form input[name="step8-form"]').on('change', () => {
+    isRadioValid()
   })
 
   // On Next Button Clicked
   $('#step8-next-button').on('click', () => {
-    // Check Validation
-    isEmailAddressValid()
-    isPhoneValid()
+    if (!isRadioValid()) return
 
-    if (isEmailAddressValid() && isPhoneValid()) {
-      goToStep9()
-    }
+    $('#lic__step8').addClass('animated faster fadeOut')
+    $('#lic__step8').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
+      $('#lic__step8').addClass('d-none')
+      $('#lic__step8').removeClass('animated faster fadeOut')
+
+      $('#lic__step9').removeClass('d-none')
+      $('#lic__step9').addClass('animated faster fadeIn')
+
+      $('#lic__step9').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
+        $('#lic__step9').removeClass('animated faster fadeIn')
+      })
+    })
+  })
+
+  // On Back Button Clicked
+  $('#step8-back-button').on('click', () => {
+    $('#lic__step8').addClass('animated faster fadeOut')
+    $('#lic__step8').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
+      $('#lic__step8').addClass('d-none')
+      $('#lic__step8').removeClass('animated faster fadeOut')
+
+      $('#lic__step7').removeClass('d-none')
+      $('#lic__step7').addClass('animated faster fadeIn')
+      $('#lic__step7').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
+        $('#lic__step7').removeClass('animated faster fadeIn')
+      })
+    })
   })
 })
 
-function goToStep9() {
-  $('#hic__step8').addClass('animated faster fadeOut')
-  $('#hic__step8').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
-    $('#hic__step8').addClass('d-none')
-    $('#hic__step8').removeClass('animated faster fadeOut')
-
-    $('#hic__step9').removeClass('d-none')
-    $('#hic__step9').addClass('animated faster fadeIn')
-
-    $('#hic__step9').one('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
-      $('#hic__step9').removeClass('animated faster fadeIn')
-
-      step9ProgressAnimation()
-    })
-  })
-}
-
-// Rule:
-// ------
-// 1. Should match this regex  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-function isEmailAddressValid() {
-  const emailAddressRegEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
-  if (!emailAddressRegEx.test($('#step8-form__email input').val())) {
-    $('#step8-form__email').addClass('error-state')
-    $('#step8-form__email + .info').removeClass('d-none')
+function isRadioValid() {
+  const isSelected = $('#step8-form input[name="step8-form"]:checked').val() !== undefined
+  if (!isSelected) {
+    $('#step8-form label.label').addClass('error-state')
+    $('#step8-form + .info').removeClass('d-none')
 
     return false
   } else {
-    $('#step8-form__email').removeClass('error-state')
-    $('#step8-form__email + .info').addClass('d-none')
+    $('#step8-form  label.label').removeClass('error-state')
+    $('#step8-form + .info').addClass('d-none')
 
     return true
   }
-}
-
-// Rule:
-// ------
-// 1. Should match this regex  /^0(2|3|4|7|8){1}[0-9]{8}$/
-function isPhoneValid() {
-  const phoneRegEx = /^0(2|3|4|7|8){1}[0-9]{8}$/
-
-  if (!phoneRegEx.test($('#step8-form__phone input').val())) {
-    $('#step8-form__phone').addClass('error-state')
-    $('#step8-form__phone + .info').removeClass('d-none')
-
-    return false
-  } else {
-    $('#step8-form__phone').removeClass('error-state')
-    $('#step8-form__phone + .info').addClass('d-none')
-
-    return true
-  }
-}
-
-function step9ProgressAnimation() {
-  setTimeout(() => {
-    $('.hic__step9 .progress .progress-bar').css('width', '0')
-    $('.hic__step9 .progress .progress-bar').css('width', '30%')
-    setTimeout(() => {
-      $('.hic__step9 .progress .progress-bar').css('width', '70%')
-      setTimeout(() => {
-        $('.hic__step9 .progress .progress-bar').css('width', '100%')
-
-        // on progress complete
-        $('#step9-firstTitle').addClass('d-none')
-        $('#step9-secondTitle').removeClass('d-none')
-        $('#step9-loading .success').removeClass('d-none')
-        $('#step9-next-button').removeClass('d-none')
-        $('#step9-loading .loading').addClass('d-none')
-        $('#step9-loading .message').html('Analysis complete')
-      }, 800)
-    }, 1000)
-  }, 500)
 }

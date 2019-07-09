77681148c5f5c25ad0cabd7b441d628981d97e2a
baadillahnabil <nabilzero27@gmail.com>
2019-07-09 14:33:32 +0700

    add company logos and make it responsive

---
+++ project-10/components/MainContent/CompanyLogos/companyLogos.js
@@ -0,0 +1,50 @@
+import $ from 'jquery'
+import 'slick-carousel'
+
+import leftArrow from '../../../static/icons/arrow_left.svg'
+import rightArrow from '../../../static/icons/arrow_right.svg'
+
+$(document).ready(() => {
+  $('#companyLogosList').slick({
+    infinite: true,
+    dots: false,
+    speed: 300,
+    rows: 2,
+    slidesToShow: 6,
+    arrows: true,
+    slidesToScroll: 1,
+    mobileFirst: true,
+    // autoplay: true,
+    prevArrow: `<img src="${leftArrow}" class="carousel-left-arrow" />`,
+    nextArrow: `<img src="${rightArrow}" class="carousel-right-arrow" />`,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          rows: 2,
+          arrows: true,
+          variableWidth: false,
+          slidesToShow: 6
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          rows: 2,
+          arrows: true,
+          variableWidth: false,
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 100,
+        settings: {
+          rows: 1,
+          slidesToShow: 2,
+          arrows: false,
+          variableWidth: true
+        }
+      }
+    ]
+  })
+})
